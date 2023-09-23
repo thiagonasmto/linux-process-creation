1b7207252c2aa83b26f598f93d07479cc299cc37
Thiago Lopes do Nascimento <thiagonasmento20@gmail.com>
2023-09-22 22:43:09 -0300

    add comments

--- src/main.c
+++ src/main.c
@@ -5,30 +5,35 @@
 #include "fibonacci.h"
 #include <stdio.h>
 
-int main(){
-	pid_t pid;
+int main() {
+    pid_t pid;
 
-	/* cria um processo filho */
-	pid = fork();
+    // Cria um processo filho
+    pid = fork();
 
-	if (pid < 0) { /* houve erro! */
-		/* exibir mensagem de erro */
-		printf("\nErro da criacao de processo filho!\n");
-		return 1;
-	}else if (pid == 0) { /* processo filho */
-		/* código exclusivo para o processo filho executar */
-		printf("\nInicializando processo filho...");
-		printf("\nMeu identificador (filho) eh o numero %d", getpid());	
-		printf("\nMinha função será gerar e exibir os termos da série de Fibonacci");
-		fibonacci();
-		printf("\nEncerrando!\n");
-	}else { /* processo pai */
-		/* pai esperará até que o processo filho termine */
-		wait(NULL);
-		printf("\nOla! Sou o processo pai!");
-		printf("\nCriei um processo filho de numero %d. Ele jah encerrou!", pid);
-		printf("\nMeu identificador (pai) eh o numero %d", getpid());	
-		printf("\nEncerrando!\n");
-	}
-	return 0;
+    if (pid < 0) {
+        // Ocorreu um erro na criação do processo filho
+        printf("\nErro na criação do processo filho!\n");
+        return 1;
+    } else if (pid == 0) {
+        // Código a ser executado exclusivamente pelo processo filho
+        printf("\nInicializando processo filho...");
+        printf("\nMeu identificador (filho) é o número %d", getpid());
+        printf("\nMinha função será gerar e exibir os termos da série de Fibonacci");
+        
+        // Chama a função que gera e exibe a série de Fibonacci
+        fibonacci();
+        
+        printf("\nEncerrando processo filho!\n");
+    } else {
+        // Código a ser executado exclusivamente pelo processo pai
+        // O pai aguardará até que o processo filho termine
+        wait(NULL);
+        printf("\nOlá! Sou o processo pai!");
+        printf("\nCriei um processo filho com número %d. Ele já encerrou!", pid);
+        printf("\nMeu identificador (pai) é o número %d", getpid());
+        printf("\nEncerrando processo pai!\n");
+    }
+    
+    return 0;
 }
